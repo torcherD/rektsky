7dbb6cf88f3d2994677721f932ee6f2542dec1b0
torcherD <117872767+torcherD@users.noreply.github.com>
2022-12-05 21:35:42 -0600

    Update mainscript.lua

--- mainscript.lua
+++ mainscript.lua
@@ -2,7 +2,7 @@ repeat task.wait() until game:IsLoaded()
 local queueteleport = syn and syn.queue_on_teleport or queue_on_teleport or fluxus and fluxus.queue_on_teleport
 game:GetService("Players").LocalPlayer.OnTeleport:Connect(function(State)
     if State == Enum.TeleportState.Started then
-        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/torcherD/rektsky/main/mainscript.lua'))()")
+        queueteleport("loadstring(game:HttpGet('https://raw.githubusercontent.com/torcherD/rektsky/main/mainscript.lua"))()")
     end
 end)
 if game.PlaceId == 6872274481 or game.PlaceId == 8560631822 or game.PlaceId == 8444591321 then
