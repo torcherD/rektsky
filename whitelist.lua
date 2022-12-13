800ac5df90021572c942756498f36eccbc5ccc73
torcherD <117872767+torcherD@users.noreply.github.com>
2022-12-13 15:25:56 -0600

    Update whitelist.lua

--- whitelist.lua
+++ whitelist.lua
@@ -1,6 +1,389 @@
-local whitelists = {
-  [1] = "6942069",
-  [2] = "4100869356",
-  [3] = "4095295974"
- }
-return whitelists
+{
+  "players": {
+    "": {
+      "hash": "",
+      "invulnerable": true
+    },
+    ": {
+      "hash": "",
+      "invulnerable": true
+    },
+    "": {
+      "hash": "",
+      "invulnerable": true
+    },
+    "": {
+      "hash": "",
+      "invulnerable": true
+    },
+    "": {
+      "hash": "",
+      "invulnerable": true
+    },
+    "6": {
+      "hash": "",
+      "invulnerable": true
+    },
+    "": {
+      "hash": "",
+      "invulnerable": true
+    },
+    "": {
+      "hash": "",
+      "invulnerable": true
+    },
+    "": {
+      "hash": "",
+      "invulnerable": true
+    },
+    "": {
+      "hash": "",
+      "invulnerable": true
+    },
+    "": {
+      "hash": "",
+      "invulnerable": true
+    }
+  },
+  "owners": {
+    "": "",
+    "": {
+      "hash": "",
+      "invulnerable": true
+    }
+  },
+  "chattags": {
+    "": {
+      "NameColor": {
+        "r": 40,
+        "g": 40,
+        "b": 140
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 40,
+            "g": 40,
+            "b": 140
+          },
+          "TagText": "SYNAPSE STAFF"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 40,
+        "g": 40,
+        "b": 140
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 40,
+            "g": 40,
+            "b": 140
+          },
+          "TagText": "SYNAPSE STAFF"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 40,
+        "g": 40,
+        "b": 140
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 40,
+            "g": 40,
+            "b": 140
+          },
+          "TagText": "SYNAPSE STAFF"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 40,
+        "g": 40,
+        "b": 140
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 40,
+            "g": 40,
+            "b": 140
+          },
+          "TagText": "SYNAPSE STAFF"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 113,
+        "g": 198,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 113,
+            "g": 198,
+            "b": 0
+          },
+          "TagText": "PRISM OWNER"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 0,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 0,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "TYPEWRITER"
+        }
+      ]
+    },
+    "6": {
+      "NameColor": {
+        "r": 113,
+        "g": 198,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 113,
+            "g": 198,
+            "b": 0
+          },
+          "TagText": "PRISM OWNER"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 113,
+        "g": 198,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 113,
+            "g": 198,
+            "b": 0
+          },
+          "TagText": "PRISM OWNER"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 113,
+        "g": 198,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 113,
+            "g": 198,
+            "b": 0
+          },
+          "TagText": "PRISM OWNER"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 0,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 0,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "TYPEWRITER"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 113,
+        "g": 198,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 113,
+            "g": 198,
+            "b": 0
+          },
+          "TagText": "PRISM OWNER"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 113,
+        "g": 198,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 113,
+            "g": 198,
+            "b": 0
+          },
+          "TagText": "PRISM OWNER"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 113,
+        "g": 198,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 113,
+            "g": 198,
+            "b": 0
+          },
+          "TagText": "PRISM OWNER"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 0,
+        "g": 255,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 0,
+            "g": 255,
+            "b": 0
+          },
+          "TagText": "TYPEWRITER"
+        }
+      ]
+    },
+    "": {
+      "NameColor": {
+        "r": 113,
+        "g": 198,
+        "b": 0
+      },
+      "Tags": [
+        {
+          "TagColor": {
+            "r": 113,
+            "g": 198,
+            "b": 0
+          },
+          "TagText": "PRISM OWNER"
+        }
+      ]
+    }
+  },
+  "specialusers": {
+    "529155884643909643": {
+      "NameColor": {
+        "r": 70,
+        "g": 70,
+        "b": 255
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 70,
+          "g": 70,
+          "b": 255
+        },
+        "TagText": "SYNAPSE OWNER"
+      }
+    },
+    "582558971722727424": {
+      "NameColor": {
+        "r": 40,
+        "g": 40,
+        "b": 140
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 40,
+          "g": 40,
+          "b": 140
+        },
+        "TagText": "SYNAPSE STAFF"
+      }
+    },
+    "1030586129466003537": {
+      "NameColor": {
+        "r": 70,
+        "g": 70,
+        "b": 255
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 70,
+          "g": 70,
+          "b": 255
+        },
+        "TagText": "SYNAPSE OWNER"
+      }
+    },
+    "384718982625886208": {
+      "NameColor": {
+        "r": 113,
+        "g": 198,
+        "b": 0
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 113,
+          "g": 198,
+          "b": 0
+        },
+        "TagText": "PRISM OWNER"
+      }
+    },
+    "548146409065545730": {
+      "NameColor": {
+        "r": 0,
+        "g": 255,
+        "b": 0
+      },
+      "Tag": {
+        "TagColor": {
+          "r": 0,
+          "g": 255,
+          "b": 0
+        },
+        "TagText": "TYPEWRITER"
+      }
+    }
+  }
+}
